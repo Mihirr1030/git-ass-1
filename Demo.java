3540046b1e9d5e44709c4b4a7246dd23ca03d5ed
mihir <mihirparmar254@gmail.com>
2023-02-17 10:17:05 +0530

    modified Demo.java

--- Demo.java
+++ Demo.java
@@ -10,7 +10,7 @@ class Demo{
         }    
     }    
     public static void main(String args[]){    
-     int count=10;    
+     int count=11;    
      System.out.print(n1+" "+n2);//printing 0 and 1    
      printFibonacci(count-2);//n-2 because 2 numbers are already printed   
     }  
